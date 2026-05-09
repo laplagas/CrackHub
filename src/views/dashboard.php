9c3c61a9ac1ce7dab615edfae828ed4363a56204
laplagas <arthurpedreo@gmail.com>
2026-05-09 14:18:13 -0300

    fix(dashboard): corrigido problema de identação na importação do script

--- src/views/dashboard.php
+++ src/views/dashboard.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/../config/authcheck.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="../assets/images/logocrackhub.png">
-    <script> src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>Crack Hub - Dashboard</title>
 </head>
 
